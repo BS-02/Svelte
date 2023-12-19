e9debc0ea747d534a899b8ae708c193787d48a07
BS-02 <127045845+BS-02@users.noreply.github.com>
2023-12-20 00:06:38 +0530

    Add files via upload

---
+++ Chapter09/02-exercise-scroll-position/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import createStore from './scroll-store';
+  const scrollPosition = createStore();
+</script>
+
+<div>Scroll position {$scrollPosition}</div>
+
+<style>
+	div {
+		position:fixed;
+	}
+</style>
---
+++ Chapter09/07-exercise-undo-redo-higher-order-store/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { writable } from 'svelte/store';
+  import undoRedo from './undo-redo';
+  const store = undoRedo(writable(''));
+</script>
+
+<input bind:value={$store} />
+<button on:click={store.undo}>Undo</button>
+<button on:click={store.redo}>Redo</button>
